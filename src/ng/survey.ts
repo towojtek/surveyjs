112177800d8b1d11251cd362aa24317183855ff2
dmitrykurmanov <kurmanov.work@gmail.com>
2017-02-21 16:10:35 +0300

    implemented abstract survey-component

--- src/ng/survey.ts
+++ src/ng/survey.ts
@@ -1,4 +1,4 @@
-import angular from 'angular';
+import angular from "angular";
 var template = require("html-loader?interpolate!val-loader!./survey.html");
 
 angular.module("ngSurvey", []);
@@ -10,6 +10,41 @@ angular.module("ngSurvey")
             model: '<'
         },
         controller: function($scope) {
+            this.$onInit = function() {
+                var me = this;
+
+                $scope.getNavBtnClasses = function(btnType) {
+                    var btnClass = me.model.css.navigation[btnType];
+                    return me.model.navigationButton + ' ' + btnClass;
+                };
+
+                $scope.completeLastPage = function() {
+                    me.model.completeLastPage();
+                };
+
+                $scope.prevPage = function() {
+                    me.model.prevPage();
+                };
+
+                $scope.nextPage = function() {
+                    me.model.nextPage();
+                };
+            };
         }
     });
 
+angular.module("ngSurvey").
+    directive('surveyComponent', $compile => {
+    return {
+        restrict: "E",
+        replace: true,
+        link: (scope, element, attributes) => {
+            var originalComponent = element;
+            scope.$watch(attributes["type"], type => {
+                type = !!type ? type : "error"; // TODO: implement survey-error component
+                var component = $compile('<survey-' + type + ' question="question" isEditMode="$ctrl.survey.isEditMode"></survey-' + type + '>')(scope);
+                originalComponent.replaceWith(component);
+            });
+        }
+    };
+});
